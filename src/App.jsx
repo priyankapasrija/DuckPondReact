2584ef6db463e6859c3194386d86c23b45d093e7
priyankapasrija <104025698+priyankapasrija@users.noreply.github.com>
2023-12-20 11:30:22 +0530

    Created react new project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// import "./App.css";s
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="bg-indigo-500">Hello World</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
